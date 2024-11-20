8fd80fbe1d0bdfaae1b8fff30569200a8c20b52b
Soares Chen <soares.chen@maybevoid.com>
2024-11-20 12:29:24 +0000

    Implement CPU proof generator

--- crates/sp1-prover-components/src/impls/proof_generators/cpu.rs
+++ crates/sp1-prover-components/src/impls/proof_generators/cpu.rs
@@ -4,28 +4,31 @@ use hermes_prover_components::traits::types::input::HasProgramInputType;
 use hermes_prover_components::traits::types::program::HasProvableProgramType;
 use hermes_prover_components::traits::types::proof::HasProofType;
 use hermes_prover_components::traits::types::prover::HasProverType;
-use sp1_sdk::{NetworkProverV2, SP1ProofWithPublicValues, SP1Stdin};
+use sp1_sdk::provers::ProofOpts;
+use sp1_sdk::{CpuProver, Prover, SP1ContextBuilder, SP1ProofWithPublicValues, SP1Stdin};
 
-use crate::traits::fields::proof_mode::HasProofMode;
+use crate::traits::fields::proof_kind::HasProofKind;
 
-pub struct GenerateSp1ProofWithNetwork;
+pub struct GenerateSp1ProofWithCpu;
 
-impl<Context, App> ProofGenerator<Context, App> for GenerateSp1ProofWithNetwork
+impl<Context, App> ProofGenerator<Context, App> for GenerateSp1ProofWithCpu
 where
-    Context: HasProverType<Prover = NetworkProverV2>
+    Context: HasProverType<Prover = CpuProver>
         + HasProvableProgramType<App, ProvableProgram = Vec<u8>>
         + HasProgramInputType<App, ProgramInput = Vec<Vec<u8>>>
         + HasProofType<App, Proof = SP1ProofWithPublicValues>
-        + HasProofMode
+        + HasProofKind
         + CanRaiseError<anyhow::Error>,
 {
     async fn generate_proof(
         context: &Context,
-        prover: &NetworkProverV2,
+        prover: &CpuProver,
         program: &Vec<u8>,
         inputs: Vec<Vec<u8>>,
     ) -> Result<Context::Proof, Context::Error> {
-        let proof_mode = context.proof_mode();
+        let proof_kind = context.proof_kind();
+
+        let (proving_key, _) = prover.setup(program);
 
         let mut stdin = SP1Stdin::new();
 
@@ -33,9 +36,14 @@ where
             stdin.write_vec(input);
         }
 
+        let options = ProofOpts::default();
+
+        let mut context_builder = SP1ContextBuilder::default();
+
+        let prove_context = context_builder.build();
+
         let proof = prover
-            .prove(program, stdin, proof_mode.clone(), None)
-            .await
+            .prove(&proving_key, stdin, options, prove_context, *proof_kind)
             .map_err(Context::raise_error)?;
 
         Ok(proof)
